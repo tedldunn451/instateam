68db2aba664775a6257616b647721749a9c13268
Ted Dunn <tldunn451@gmail.com>
2017-03-12 11:46:44 -0400

    Add configuration files

---
+++ src/main/java/dunn/ted/java/instateam/config/AppConfig.java
@@ -0,0 +1,23 @@
+package dunn.ted.java.instateam.config;
+
+import org.hashids.Hashids;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+/**
+ * Created by Ted on 3/12/2017.
+ */
+@Configuration
+@PropertySource("app.properties")
+public class AppConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public Hashids hashids() {
+        return new Hashids(env.getProperty("instateam.hash.salt"), 8);
+    }
+}
---
+++ src/main/java/dunn/ted/java/instateam/config/DataConfig.java
@@ -0,0 +1,50 @@
+package dunn.ted.java.instateam.config;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("app.properties")
+/**
+ * Created by Ted on 3/12/2017.
+ */
+public class DataConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+
+        Resource config = new ClassPathResource("hibernate.cfg.xml");
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        sessionFactory.setConfigLocation(config);
+        sessionFactory.setPackagesToScan(env.getProperty("instateam.entity.package"));
+        sessionFactory.setDataSource(dataSource());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        BasicDataSource bds = new BasicDataSource();
+
+        bds.setDriverClassName(env.getProperty("instateam.db.driver"));
+        bds.setUrl(env.getProperty("instateam.db.url"));
+        bds.setUsername(env.getProperty("instateam.db.username"));
+        bds.setPassword(env.getProperty("instateam.db.password"));
+
+        return bds;
+    }
+}
+
